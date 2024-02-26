c155b80febddc478bae0d9076e5aba2b344090ad
Hiroto2002 <hiroto2002h14@icloud.com>
2024-02-26 23:31:54 +0900

    feat:products frame

---
+++ src/app/products/page.tsx
@@ -0,0 +1,43 @@
+import { GlassChildCardBox } from "@/components/ui/GlassChildCardBox";
+import { PRODUCTS } from "@/constants/product";
+import React from "react";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <main className="flex flex-col items-center w-[94vw] mx-auto ">
+      <GlassChildCardBox>
+        <p
+          className="font-semibold text-xl text-center"
+          style={{ fontSize: "1.3rem", color: "rgba( 255, 212, 0)" }}
+        >
+          作品
+        </p>
+      </GlassChildCardBox>
+      <section
+        className="flex flex-wrap justify-around items-center gap-4 m-auto"
+        style={{ width: "calc(94vw - 2vh)" }}
+      >
+        {PRODUCTS.map((product) => (
+          <GlassChildCardBox key={product.title} width="45%">
+            <article className="flex flex-row justify-center items-center gap-4">
+              <Image
+                src={`/images/icons/product/${product.src}`}
+                alt={product.title}
+                width={100}
+                height={100}
+                className="object-contain m-auto w-[100px] h-[100px]"
+              />
+              <p className="w-fit font-semibold m-auto">{product.title}</p>
+            </article>
+          </GlassChildCardBox>
+        ))}
+        {PRODUCTS.length % 2 !== 0 && (
+          <div className="w-[45%]"></div>
+        )}
+      </section>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/constants/product.ts
@@ -0,0 +1,27 @@
+export const PRODUCTS = [
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  {
+    title: "hack portal",
+    src: "hackportal.png",
+  },
+  
+];
